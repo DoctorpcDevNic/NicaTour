39f13ea27aad60e2ff546c55d39e8f31720a9b0a
kher09 <kher09@outlook.com>
2015-02-02 22:29:03 -0600

    Menu Depto
    - Menu depto mejorado
    - Vista hoteles y restaurantes mejoradas
    - Widget clima mejorado

--- app/views/lenguajes/es/departamentos/restaurantes.blade.php
+++ app/views/lenguajes/es/departamentos/restaurantes.blade.php
@@ -27,9 +27,10 @@ Restaurantes en {{$depto->nombre}} - Nicaragua Tour
 	@if(!count($Rest)==0)
 		<div class="row">
 			@foreach($Rest as $key)
-				<div class="col-sm-6 col-xs-6 restaurant">
+				<div class="col-sm-6 col-xs-12 restaurant">
 					<div class="col-xs-12 resttitul">
-						<h3>{{ $key->nombre }} <img class="pull-right" src="{{ asset('img/Restaurante-01.png') }}"></h3>
+						<h3>{{ $key->nombre }}</h3>
+						<img class="hidden-xs" src="{{ asset('img/Restaurante-01.png') }}">
 					</div>
 					<div class="col-xs-12 restdescripcion">
 						<p><i class="fa fa-phone fa-fw"></i>{{ $key->telefono }}</p>
--- climacron1.php
+++ climacron1.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM infoclima";
+$sql = "SELECT * FROM deptos";
 $result = $conn->query($sql);
 $conteo=0;
 
@@ -20,28 +20,31 @@ $conteo=0;
 	    // output data of each row
 	    while($row = $result->fetch_assoc())
 	    {
-	        if($row["id_dpto"]<9)
+	        if($row["id"]<9)
 	        {	
-	        	  $json_string = file_get_contents($row["url"]);
+	        	  $json_string = file_get_contents($row["urlclima"]);
 				  $parsed_json = json_decode($json_string);
 				  $location = $parsed_json->{'current_observation'}->{'display_location'}->{'city'};
-				  $temp_c = $parsed_json->{'current_observation'}->{'temp_c'};
-				  $icono = $parsed_json->{'current_observation'}->{'icon_url'};
+				  $temp_c = round($parsed_json->{'current_observation'}->{'temp_c'});
+				  $icono = $parsed_json->{'current_observation'}->{'icon'};
 				  $altura = $parsed_json->{'current_observation'}->{'observation_location'}->{'elevation'};
 				  $humedad = $parsed_json->{'current_observation'}->{'relative_humidity'};
 				  
 
-				  $actualizar="UPDATE detalleclima SET temperatura='".$temp_c."', icono='".$icono."', humedad='".$humedad."' WHERE id_depto=".$row["id_dpto"];
+				  $actualizar="UPDATE deptos SET municipio='".$location."', temperatura='".$temp_c."', icono='".$icono."', humedad='".$humedad."' WHERE id=".$row["id"];
 
 				  if ($conn->query($actualizar) === TRUE) {
-				  		$conteo++;					   
+				  		$conteo++;	
+						echo "Datos climaticos de ".$location." actualizados";
+						echo"\n";
 					} else {
-					    echo "Error updating record: " . $conn->error;
+					    echo "Error actualizando registro: " . $conn->error."\n";
+						echo "Error al actualizar datos climaticos de ".$location;
 					}
 
 	        }	        
 	    }	    
-	    echo $conteo." Registros actualizados";
+	    echo $conteo." Registros actualizados en total";
 	}
 	
 	else {
